d8b5e4948ec997f398a3ad7c977c5cd8a168f7db
NadyaBurnos <153112000+NadyaBurnos@users.noreply.github.com>
2025-01-19 22:19:46 +0700

    Update task.js

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,17 @@
+const dropdown = document.querySelectorAll('.dropdown');
+const value = document.querySelector('.dropdown__value');
+const list = document.querySelector('.dropdown__list');
+const items = document.querySelectorAll('.dropdown__item');
+
+value.addEventListener('click', () => {
+    list.classList.toggle('dropdown__list_active');
+});
+
+items.forEach(item => {
+    item.addEventListener('click', (a) => {
+        a.preventDefault(); 
+
+        list.classList.remove('dropdown__list_active');
+        value.textContent = item.textContent.trim();
+    });
+});
