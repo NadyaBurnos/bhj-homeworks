521b2776f87e5162410aa68ad8c4405f3cba78c0
NadyaBurnos <153112000+NadyaBurnos@users.noreply.github.com>
2025-02-10 20:51:32 +0700

    Update task.js

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,9 +2,10 @@ const reveal = document.querySelectorAll('.reveal');
 
 window.addEventListener('scroll', function() {
     reveal.forEach((el) => {
-        el.classList.remove('reveal_active');
         if (el.getBoundingClientRect().top < window.innerHeight) {
             el.classList.add('reveal_active');
+        } else {
+            el.classList.remove('reveal_active');
         }
     });
 });
