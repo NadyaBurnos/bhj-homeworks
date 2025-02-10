badac3da3e2edf0d952b2bbb452e4572a25c06b1
NadyaBurnos <153112000+NadyaBurnos@users.noreply.github.com>
2025-02-10 20:53:01 +0700

    Update task.js

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -4,6 +4,10 @@ const book = document.getElementById('book');
 fontSize.forEach((size) => {
     size.addEventListener('click', (event) => {
         event.preventDefault();
+
+        fontSize.forEach(e => e.classList.remove('font-size_active'));
+        event.target.classList.add('font-size_active');
+
         book.classList.remove('book_fs-big', 'book_fs-small');
         if (event.target.dataset.size === 'small') {
             book.classList.add('book_fs-small');
